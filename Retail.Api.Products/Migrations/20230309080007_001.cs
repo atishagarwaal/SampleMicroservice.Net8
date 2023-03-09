7a601e6f160cd459909b8dcfe979db5408b72d32
aatishagarwal <aatishagarwal@live.com>
2023-03-09 13:30:33 +0530

    Migration run

---
+++ Retail.Api.Products/Migrations/20230309080007_001.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Retail.Api.Products.Migrations
+{
+    /// <inheritdoc />
+    public partial class _001 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Skus",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UnitPrice = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skus", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Skus",
+                columns: new[] { "Id", "Name", "UnitPrice" },
+                values: new object[,]
+                {
+                    { 1L, "Rice", 50.0 },
+                    { 2L, "Salt", 30.0 },
+                    { 3L, "Sugar", 60.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Skus");
+        }
+    }
+}
