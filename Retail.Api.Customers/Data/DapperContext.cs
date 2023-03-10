c6ed89197a7363ac188cb65b995af84f045add5f
aatishagarwal <aatishagarwal@live.com>
2023-03-10 14:37:07 +0530

    Add Dapper

---
+++ Retail.Api.Customers/Data/DapperContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Retail.Api.Customers.Data
+{
+    /// <summary>
+    /// Dapper context class.
+    /// </summary>
+    public class DapperContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DapperContext"/> class.
+        /// </summary>
+        /// <param name="configuration">Configuration object.</param>
+        public DapperContext(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets Configuration.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Sets SQL Connection.
+        /// </summary>
+        /// <returns>Returns Sql connection.</returns>
+        public IDbConnection CreateConnection() => new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
+    }
+}
