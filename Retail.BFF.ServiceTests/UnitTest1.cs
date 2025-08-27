4b2e605977255f5f688d3c6af0c536dd0628a4d0
Aatish Agarwal <aagarwal@symbotic.com>
2025-08-27 23:00:14 +0530

    Added new test projects

---
+++ Retail.BFF.ServiceTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Retail.BFF.ServiceTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Retail.BFF.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Retail.BFF.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
